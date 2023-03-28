ab33576dfd155e2af0ec64f549c427d69e50494c
L M <80474693+Loris-Moreau@users.noreply.github.com>
2023-03-28 15:02:37 +0200

    [fix]

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Detection detection;
     ThirdPersonController TPC;
 
     public static Player Instance;
 
     public GameObject loseText;
     public GameObject winText;
-
-    public bool canBackStab = false;
 
     void Awake()
     {
@@ -24,41 +21,19 @@ public class Player : MonoBehaviour
     private void Start()
     {
         TPC = GetComponent<ThirdPersonController>();
-
-        /*loseText.SetActive(false);
-        winText.SetActive(false);*/
     }
 
     void Update()
     {
-        if (detection && detection.playerDetected)
-        {
-            detection.enemySpeed = 0f;
-            TPC.MoveSpeed = 0f;
-            loseText.SetActive(true);
-        }
+      
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (gameObject.CompareTag("winZone"))
-        {
-            TPC.MoveSpeed = 0f;
-            winText.SetActive(true);
-        }
-
         if (gameObject.CompareTag("Enemy"))
         {
-            if(canBackStab)
-            {
-                
-                //kill enemy
-            }
+           //Enemy hit
         }
-    }
-
-    public void OnDetection(Detection detector)
-    {
-        detection = detector;
     }
 }
 

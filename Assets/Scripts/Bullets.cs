e269da2be321530af034a95911700d8babd3892b
L M <80474693+Loris-Moreau@users.noreply.github.com>
2023-03-29 18:27:04 +0200

    cleanup

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+	public GameObject boom;
+	public GameObject boomS;
+
+    public int damage;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Enemy"))
+		{
+			boomS.SetActive(true);
+			boom.SetActive(true); //Kaboom
+		}
+	}
+}
---
+++ Assets/Scripts/Sling.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sling : MonoBehaviour
+{
+	public Animator slingAnim;
+
+	float charge; //curent charge
+	public float chargeMax; 
+	public float chargeSpeed; //rate of the charge (how fast it goes)
+
+	public KeyCode fireButton; //boom boom button (bbb)
+
+	public Transform spawn; //position of the bullet
+	public Rigidbody buulletRb; //"blueprint" of the bullet
+
+	private void Start()
+	{
+		//slingAnim = GetComponent<Animator>();	
+	}
+
+	private void Update()
+	{
+		if(Input.GetKey(fireButton) && charge < chargeMax) //pressing bbb & charge smaller than max charge
+		{
+			charge += Time.deltaTime * chargeSpeed; //charging up
+			Debug.Log(charge.ToString());
+			slingAnim.SetBool("Aim", true); //sling shot pull
+		}
+
+		if (Input.GetKeyUp(fireButton))
+		{
+			Rigidbody bullet = Instantiate(buulletRb, spawn.position, Quaternion.identity) as Rigidbody; //make new bullet at bullet spawn, as rigidbody not an object
+			bullet.AddForce(spawn.forward * (charge), ForceMode.Impulse); //shoots the bullet forward multiplied by the charge, ///forceMode aply force all at once 
+			charge = 0; //reset charge to fire again
+			slingAnim.SetBool("Aim", false); //sling shot release
+		}
+	}
+}

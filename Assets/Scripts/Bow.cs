29175a09a3a78133d366dd64b796347a788a63c1
L M <80474693+Loris-Moreau@users.noreply.github.com>
2023-03-29 02:52:31 +0200

    Sling Shot
    nyoom

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
+	public Animator slingAnim;
 	float charge; //curent charge
 	public float chargeMax; 
 	public float chargeSpeed; //rate of the charge (how fast it goes)
@@ -12,6 +13,10 @@ public class Bow : MonoBehaviour
 
 	public Transform spawn; //position of the arrow
 	public Rigidbody arrowRb; //"blueprint" of the arrow
+	private void Start()
+	{
+		slingAnim = GetComponent<Animator>();	
+	}
 
 	private void Update()
 	{
@@ -19,6 +24,7 @@ public class Bow : MonoBehaviour
 		{
 			charge += Time.deltaTime * chargeSpeed; //charging up
 			Debug.Log(charge.ToString());
+			slingAnim.SetBool("Aim", true); //sling shot pull
 		}
 
 		if (Input.GetKeyUp(fireButton))
@@ -26,6 +32,7 @@ public class Bow : MonoBehaviour
 			Rigidbody arrow = Instantiate(arrowRb, spawn.position, Quaternion.identity) as Rigidbody; //make new arrow at arrow spawn, as rigidbody not an object
 			arrow.AddForce(spawn.forward * charge, ForceMode.Impulse); //shoots the arrow forward multiplied by the charge, ///forceMode aply force all at once 
 			charge = 0; //reset charge to fire again
+			slingAnim.SetBool("Aim", true); //sling shot release
 		}
 	}
 }

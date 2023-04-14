49e7cf8627945c6852cd292abc4403799661955d
L M <80474693+Loris-Moreau@users.noreply.github.com>
2023-04-14 18:05:57 +0200

    skelly

--- Assets/Scripts/Sling.cs
+++ Assets/Scripts/Sling.cs
@@ -25,14 +25,18 @@ public class Sling : MonoBehaviour
 		if(Input.GetKey(fireButton) && charge < chargeMax) //pressing bbb & charge smaller than max charge
 		{
 			charge += Time.deltaTime * chargeSpeed; //charging up
+
 			Debug.Log(charge.ToString());
+
 			slingAnim.SetBool("Aim", true); //sling shot pull
 		}
 
 		if (Input.GetKeyUp(fireButton))
 		{
 			Rigidbody bullet = Instantiate(buulletRb, spawn.position, Quaternion.identity) as Rigidbody; //make new bullet at bullet spawn, as rigidbody not an object
+
 			bullet.AddForce(spawn.forward * (charge), ForceMode.Impulse); //shoots the bullet forward multiplied by the charge, ///forceMode aply force all at once 
+
 			charge = 0; //reset charge to fire again
 			slingAnim.SetBool("Aim", false); //sling shot release
 		}
